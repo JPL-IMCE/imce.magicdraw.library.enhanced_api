19e8a31db0f4f4da54d5be743bc355cb540faee7
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-02-02 14:35:24 +0000

    dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
   // this project
-  val version = "2.1"
+  val version = "2.2"
 
   // https://github.jpl.nasa.gov/imce/jpl-mbee-common-scala-libraries
-  val jpl_mbee_common_scala_libraries="1.11"
+  val jpl_mbee_common_scala_libraries="1.12"
 
   // https://github.jpl.nasa.gov/mbee-dev/cae.magicdraw.package.mdk
   val mdk_package = "2.3-RC3"
