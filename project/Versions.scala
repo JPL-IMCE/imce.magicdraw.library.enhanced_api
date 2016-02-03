f735dddd9b883003046650ce4e058f356eb5c151
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-02-03 05:41:28 +0000

    Migrated to the imce.third_party.*_libraries

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
   // this project
-  val version = "2.5"
+  val version = "3.0"
 
-  // https://github.jpl.nasa.gov/imce/jpl-mbee-common-scala-libraries
-  val jpl_mbee_common_scala_libraries="1.14"
+  // https://github.jpl.nasa.gov/imce/imce.third_party.aspectj_libraries
+  val aspectj_libraries="1.0"
 
   // https://github.jpl.nasa.gov/mbee-dev/cae.magicdraw.package.mdk
   val mdk_package = "2.3-RC4"
