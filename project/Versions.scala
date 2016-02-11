0e25afae263bee8baf40f58f8ebab8c615ae0228
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-02-11 15:37:18 +0000

    CI

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
 
   // this project
-  val version = "4.2.3"
+  val version = "4.2.4"
 
   // https://github.jpl.nasa.gov/imce/imce.third_party.aspectj_libraries/blob/master/project/Versions.scala#L11
   val org_aspectj_version = "1.8.7"
